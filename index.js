1b96c7f89ed7ec3b5f0e089b939de15a665c3dc8
Jeffchua96 <jeffersonchua@pursuit.org>
2023-03-26 05:08:07 -0400

    lab event form

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Code your solution!")
+console.log("Code your solution")
+
+let form = document.querySelector("form");
+
+const button.document.querySelector("input")
+
+ button.addEventListener('click', updateButton);
+ const paragraph =document.querySelector("p")
+ p.textarea= "Error.";
+
+let ul = document.querySelector("ul");
+ form.addEventListener("submit", (event) => {
+ event.preventDefault();
+
+ let li=document.createElement("li");
+ li.textarea=event.target.todo.
+ ul.append("Todo")
+ 
+    li.addEventListener("click", () =>{
+      li.style.textDecoration= "overline"
+    });
+})
+  
+ 
+ 
